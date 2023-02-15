1b0a911d9b15060d2ddc35331d5319b1004aa581
Anderson Cristiano F. Machado <65175553+acristianno@users.noreply.github.com>
2023-02-15 00:02:03 -0300

    CSS and DOM classes
    studying dom and css, adding the lottery exercise

---
+++ Minicamp - Programção Basica/Modulo 2/Projeto/06-imc/js/script.js
@@ -0,0 +1,32 @@
+function start() {
+  var buttonCalculateImc = document.querySelector('#Button-calculate-imc');
+  buttonCalculateImc.addEventListener('click', handleButtonClick);
+
+  var inputWeight = document.querySelector('#input-weight');
+  var inputHeight = document.querySelector('#input-height');
+
+  inputHeight.addEventListener('input', handleButtonClick);
+  inputWeight.addEventListener('input', handleButtonClick);
+
+  handleButtonClick();
+}
+
+function calculateImc(weight, height) {
+  return weight / (height * height);
+}
+
+function handleButtonClick() {
+  var inputWeight = document.querySelector('#input-weight');
+  var inputHeight = document.querySelector('#input-height');
+  var imcResult = document.querySelector('#imc-result');
+
+  var weight = inputWeight.value;
+  var height = inputHeight.value;
+
+  var imc = calculateImc(weight, height);
+  var formattedImc = imc.toFixed(2).replace('.', ',');
+
+  imcResult.textContent = formattedImc;
+}
+
+start();
